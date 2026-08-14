942d0effc56a6d4883b640be745b615e1b6d632a
Ritesh031983 <ritesh.bahety1983@gmail.com>
2026-08-14 07:39:22 +0530

    fixing app build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.ritesh.bmi.calculator"
         minSdk = 31
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
